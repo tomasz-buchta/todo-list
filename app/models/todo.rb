ab8d9e39b2c43f46de1253955f043f55980c7cb8
Tomasz Buchta <tomasz.buchta@hotmail.com>
2015-03-20 07:53:03 +0100

    Added timestamps to todo model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,6 @@
 class Todo
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :title, type: String
   field :description, type: String
   field :completion, type: Integer
@@ -7,4 +8,13 @@ class Todo
   validates :title, presence: true
   validates :completion, presence: true
   validates :completion, numericality: {greater_than: 0,less_than: 100}
+
+  after_initialize :add_created_at
+
+  def add_created_at
+    unless self.created_at.present?
+      self.created_at = self.id.generation_time
+      self.save
+    end
+  end
 end
