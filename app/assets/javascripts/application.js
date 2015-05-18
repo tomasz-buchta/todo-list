5f16b8edc63b2a956dced587d74ad48cb69b88d3
Tomasz Buchta <tomasz.buchta@polcode.net>
2015-05-18 13:02:09 +0200

    Angular is now loaded from assets pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require foundation
+//= require angular/angular
 //= require angular-route/angular-route
 //= require angular-rails-templates
 //= require_tree ./templates
