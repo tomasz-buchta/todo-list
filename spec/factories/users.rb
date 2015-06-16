12b9da3583ec20da73d774f3c178064dc4d03d51
Tomasz Buchta <tomasz.buchta@polcode.net>
2015-06-16 11:05:25 +0200

    wip

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password 'xsw2zaq1'
+  end
+
+  factory :confirmed_user do
+    confirmet_at Time.zone.now
   end
 
 end
